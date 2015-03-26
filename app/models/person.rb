a37738b35b25c1d8c1717e70f4fbb3192b847655
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-03-26 15:11:01 +0800

    email should be identical and not empty.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,9 @@
 class Person < ActiveRecord::Base
   has_and_belongs_to_many :events
   has_many :costs
+
+  # http://guides.rubyonrails.org/active_record_validations.html
+  validates :name, :email, :money, presence: true
+  validates :email, uniqueness: true
+
 end
