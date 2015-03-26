d4a5d63b585eaae703ee14cd0bfc35d0844d14e1
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-03-26 16:08:46 +0800

    validate cost and event model.

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,4 +1,6 @@
 class Cost < ActiveRecord::Base
   belongs_to :event
   belongs_to :person
+
+  validates :money, :person_id, :event_id, :att_nr, presence: true
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :people
   has_many :costs
+  validates :cost_total, :payer_id, :att_nr, presence: true
 end
